c27a6bb2e4e785e8734e23becab422eb0f6da1a6
Daniel Gehn <inad.gamer@gmail.com>
2018-02-18 00:38:50 +0100

    added arte provider see #1

---
+++ src/mediatheken/Arte.php
@@ -0,0 +1,157 @@
+<?php
+
+require_once dirname(__FILE__) . '/../utils/Mediathek.php';
+require_once dirname(__FILE__) . '/../utils/Result.php';
+
+/**
+ * @author Daniel Gehn <me@theinad.com>
+ * @copyright 2018 Daniel Gehn
+ * @license http://opensource.org/licenses/MIT Licensed under MIT License
+ */
+class Arte extends Mediathek
+{
+
+    protected static $LANGUAGE_MAP = array(
+        'de' => 'de',
+        'fr' => 'fr',
+    );
+    protected static $LANGUAGE_MAP_SHORT_LIBELLE = array(
+        'de' => 'de',
+        'fr' => ['vf', 'vof'],
+    );
+    protected static $OV_SHORT_LIBELLE = array(
+        'og',
+        'ov',
+        'omu',
+        'vostf'
+    );
+
+    protected $supportMatcher = 'arte.tv';
+
+    protected $language = 'de';
+    protected $languageShortLibelle = 'de';
+
+    protected $platform = null;
+    protected $detectedLanguage = null;
+    protected $subdomain = null;
+
+    public function getDownloadInfo($url, $username = '', $password = '')
+    {
+        $this->getLogger()->log('determining website and language by url ' . $url);
+        if ($this->extractLanguageAndPlatformFromUrl($url) === false) {
+            return null;
+        }
+
+        $this->changeLanguageIfDetected();
+
+        $this->getLogger()->log('using language ' . $this->language . ' (' . print_r($this->languageShortLibelle, true) . ').');
+
+        $this->getLogger()->log('fetching page content.');
+
+        $videoPage = $this->getTools()->curlRequest($url);
+        if ($videoPage === null) {
+            return null;
+        }
+
+        $json = $this->processVideoPage($videoPage);
+        if ($json === null) {
+            return null;
+        }
+
+        $result = $this->getBestSource($json);
+        if (!$result->hasUri()) {
+            return null;
+        }
+
+        $result = $this->addTitle($result, $json);
+        return $result;
+    }
+
+    protected function extractLanguageAndPlatformFromUrl($url)
+    {
+        if (preg_match('#https?:\/\/(\w+\.)?arte.tv\/(?:guide\/)?([a-zA-Z]+)#si', $url, $match) > 0) {
+            $this->platform = 'arte';
+            $this->subdomain = $match[1];
+            $this->detectedLanguage = isset($match[2]) ? $match[2] : null;
+            return true;
+        }
+
+        $this->getLogger()->log('not a known arte website');
+        return false;
+    }
+
+    protected function changeLanguageIfDetected()
+    {
+        if (
+            $this->detectedLanguage !== null && 
+            isset(self::$LANGUAGE_MAP[$this->detectedLanguage]) && 
+            isset(self::$LANGUAGE_MAP_SHORT_LIBELLE[$this->detectedLanguage])
+        ) {
+            $this->language = self::$LANGUAGE_MAP[$this->detectedLanguage];
+            $this->languageShortLibelle = self::$LANGUAGE_MAP_SHORT_LIBELLE[$this->detectedLanguage];
+        }
+    }
+
+    protected function processVideoPage($videoPage)
+    {
+        if (preg_match('#src=["|\']http.*?json_url=(.*?)%3F.*["|\']#si', $videoPage, $match) === 1) {
+            $playerUrl = urldecode($match[1]);
+            $this->getLogger()->log('the player is located at ' . $playerUrl);
+            $json = $this->getTools()->curlRequest($playerUrl);
+            if ($json === null) {
+                return null;
+            }
+
+            return json_decode($json);
+        }
+
+        $this->getLogger()->log('could not identify player meta.');
+        return null;
+    }
+
+    protected function getBestSource($json)
+    {
+        $result = new Result();
+
+        foreach ($json->videoJsonPlayer->VSR as $source) {
+            $this->getLogger()->log("found quality of $source->bitrate with language $source->versionLibelle ($source->versionShortLibelle)");
+            $shortLibelleLowercase = mb_strtolower($source->versionShortLibelle);
+            if (
+                $source->mediaType == "mp4" &&
+                (
+                    $this->shortLibelleMatches($shortLibelleLowercase) ||
+                    $this->shortLibelleIsOv($shortLibelleLowercase)
+                ) &&
+                $source->bitrate > $result->getBitrateRating()
+            ) {
+                $result = new Result();
+
+                $result->setBitrateRating($source->bitrate);
+                $result->setUri($source->url);
+            }
+        }
+
+        return $result;
+    }
+
+    protected function shortLibelleMatches($shortLibelle)
+    {
+        if (!is_array($this->languageShortLibelle)) {
+            return $shortLibelle === $this->languageShortLibelle;
+        }
+        
+        return in_array($shortLibelle, $this->languageShortLibelle);
+    }
+
+    protected function shortLibelleIsOv($shortLibelle)
+    {
+        return in_array($shortLibelle, self::$OV_SHORT_LIBELLE);
+    }
+
+    protected function addTitle($result, $json)
+    {
+        $result->setTitle(trim($json->videoJsonPlayer->VTI));
+        $result->setEpisodeTitle(isset($json->videoJsonPlayer->VSU) ? trim($json->videoJsonPlayer->VSU) : '');
+        return $result;
+    }
+}
---
+++ tests/ArteTest.php
@@ -0,0 +1,184 @@
+<?php
+
+final class ArteTest extends TestCase
+{
+    public function testDownloadInfoCanBeRetrievedFromValidDeUrlOriginalWithSubtitles(): void
+    {
+        $VALID_DOWNLOAD_URL = 'https://www.arte.tv/de/videos/073465-007-A/berlin-live-mike-the-mechanics/';
+        $API_URL = 'https://api.arte.tv/api/player/v1/config/de/073465-007-A';
+        $MEDIA_FILE_URL = 'https://arteconcert-a.akamaihd.net/am/concert/073000/073400/073465-007-A_SQ_0_VO-STF_03506192_MP4-2200_AMM-CONCERT-NEXT_uS8REPww5.mp4';
+
+        $logger = $this->createMock(Logger::class);
+        $curl = $this->createMock(Curl::class);
+        $tools = new Tools($logger, $curl);
+
+        $curl->expects($this->exactly(2))
+        ->method('request')
+        ->withConsecutive(
+            [$this->equalTo($VALID_DOWNLOAD_URL)],
+            [$this->equalTo($API_URL)]
+        )
+        ->willReturnOnConsecutiveCalls(
+            $this->getFixture('arte/de/ows/videoPage.html'),
+            $this->getFixture('arte/de/ows/apiResponse.json')
+        );
+
+        $arte = new Arte($logger, $tools);
+        $result = $arte->getDownloadInfo($VALID_DOWNLOAD_URL);
+
+        $this->assertInstanceOf(Result::class, $result);
+        $this->assertEquals($MEDIA_FILE_URL, $result->getUri());
+        $this->assertEquals('Berlin Live: Mike + The Mechanics', $result->getTitle());
+        $this->assertEquals('', $result->getEpisodeTitle());
+    }
+
+    public function testDownloadInfoCanBeRetrievedFromValidDeUrlWithGermanOrigin(): void
+    {
+        $VALID_DOWNLOAD_URL = 'https://www.arte.tv/de/videos/080997-000-A/spaete-aufarbeitung/';
+        $API_URL = 'https://api.arte.tv/api/player/v1/config/de/080997-000-A';
+        $MEDIA_FILE_URL = 'https://arteptweb-a.akamaihd.net/am/ptweb/080000/080900/080997-000-A_SQ_0_VOA-STA_03502346_MP4-2200_AMM-PTWEB_uNbkEDM6T.mp4';
+
+        $logger = $this->createMock(Logger::class);
+        $curl = $this->createMock(Curl::class);
+        $tools = new Tools($logger, $curl);
+
+        $curl->expects($this->exactly(2))
+        ->method('request')
+        ->withConsecutive(
+            [$this->equalTo($VALID_DOWNLOAD_URL)],
+            [$this->equalTo($API_URL)]
+        )
+        ->willReturnOnConsecutiveCalls(
+            $this->getFixture('arte/de/originDe/videoPage.html'),
+            $this->getFixture('arte/de/originDe/apiResponse.json')
+        );
+
+        $arte = new Arte($logger, $tools);
+        $result = $arte->getDownloadInfo($VALID_DOWNLOAD_URL);
+
+        $this->assertInstanceOf(Result::class, $result);
+        $this->assertEquals($MEDIA_FILE_URL, $result->getUri());
+        $this->assertEquals('Späte Aufarbeitung', $result->getTitle());
+        $this->assertEquals('', $result->getEpisodeTitle());
+    }
+
+    public function testDownloadInfoCanBeRetrievedFromValidDeUrlWithFrenchOrigin(): void
+    {
+        $VALID_DOWNLOAD_URL = 'https://www.arte.tv/de/videos/072310-040-A/karambolage/';
+        $API_URL = 'https://api.arte.tv/api/player/v1/config/de/072310-040-A';
+        $MEDIA_FILE_URL = 'https://arteptweb-a.akamaihd.net/am/ptweb/072000/072300/072310-040-A_SQ_0_VA_03389076_MP4-2200_AMM-PTWEB_sPpT1Jgi58.mp4';
+
+        $logger = $this->createMock(Logger::class);
+        $curl = $this->createMock(Curl::class);
+        $tools = new Tools($logger, $curl);
+
+        $curl->expects($this->exactly(2))
+        ->method('request')
+        ->withConsecutive(
+            [$this->equalTo($VALID_DOWNLOAD_URL)],
+            [$this->equalTo($API_URL)]
+        )
+        ->willReturnOnConsecutiveCalls(
+            $this->getFixture('arte/de/originFr/videoPage.html'),
+            $this->getFixture('arte/de/originFr/apiResponse.json')
+        );
+
+        $arte = new Arte($logger, $tools);
+        $result = $arte->getDownloadInfo($VALID_DOWNLOAD_URL);
+
+        $this->assertInstanceOf(Result::class, $result);
+        $this->assertEquals($MEDIA_FILE_URL, $result->getUri());
+        $this->assertEquals('Karambolage', $result->getTitle());
+        $this->assertEquals('', $result->getEpisodeTitle());
+    }
+
+    public function testDownloadInfoCanBeRetrievedFromValidFrUrlOriginalWithSubtitles(): void
+    {
+        $VALID_DOWNLOAD_URL = 'https://www.arte.tv/fr/videos/073465-007-A/berlin-live-mike-the-mechanics/';
+        $API_URL = 'https://api.arte.tv/api/player/v1/config/fr/073465-007-A';
+        $MEDIA_FILE_URL = 'https://arteconcert-a.akamaihd.net/am/concert/073000/073400/073465-007-A_SQ_0_VO-STF_03506192_MP4-2200_AMM-CONCERT-NEXT_uS8REPww5.mp4';
+
+        $logger = $this->createMock(Logger::class);
+        $curl = $this->createMock(Curl::class);
+        $tools = new Tools($logger, $curl);
+
+        $curl->expects($this->exactly(2))
+        ->method('request')
+        ->withConsecutive(
+            [$this->equalTo($VALID_DOWNLOAD_URL)],
+            [$this->equalTo($API_URL)]
+        )
+        ->willReturnOnConsecutiveCalls(
+            $this->getFixture('arte/fr/ows/videoPage.html'),
+            $this->getFixture('arte/fr/ows/apiResponse.json')
+        );
+
+        $arte = new Arte($logger, $tools);
+        $result = $arte->getDownloadInfo($VALID_DOWNLOAD_URL);
+
+        $this->assertInstanceOf(Result::class, $result);
+        $this->assertEquals($MEDIA_FILE_URL, $result->getUri());
+        $this->assertEquals('Berlin Live : Mike + The Mechanics', $result->getTitle());
+        $this->assertEquals('', $result->getEpisodeTitle());
+    }
+
+    public function testDownloadInfoCanBeRetrievedFromValidFrUrlWithGermanOrigin(): void
+    {
+        $VALID_DOWNLOAD_URL = 'https://www.arte.tv/fr/videos/080997-000-A/franco-vu-de-l-etranger/';
+        $API_URL = 'https://api.arte.tv/api/player/v1/config/fr/080997-000-A';
+        $MEDIA_FILE_URL = 'https://arteptweb-a.akamaihd.net/am/ptweb/080000/080900/080997-000-A_SQ_0_VF-STF_03502342_MP4-2200_AMM-PTWEB_uNZpEDLwk.mp4';
+
+        $logger = $this->createMock(Logger::class);
+        $curl = $this->createMock(Curl::class);
+        $tools = new Tools($logger, $curl);
+
+        $curl->expects($this->exactly(2))
+        ->method('request')
+        ->withConsecutive(
+            [$this->equalTo($VALID_DOWNLOAD_URL)],
+            [$this->equalTo($API_URL)]
+        )
+        ->willReturnOnConsecutiveCalls(
+            $this->getFixture('arte/fr/originDe/videoPage.html'),
+            $this->getFixture('arte/fr/originDe/apiResponse.json')
+        );
+
+        $arte = new Arte($logger, $tools);
+        $result = $arte->getDownloadInfo($VALID_DOWNLOAD_URL);
+
+        $this->assertInstanceOf(Result::class, $result);
+        $this->assertEquals($MEDIA_FILE_URL, $result->getUri());
+        $this->assertEquals("Franco vu de l'étranger", $result->getTitle());
+        $this->assertEquals('', $result->getEpisodeTitle());
+    }
+
+    public function testDownloadInfoCanBeRetrievedFromValidFrUrlWithFrenchOrigin(): void
+    {
+        $VALID_DOWNLOAD_URL = 'https://www.arte.tv/fr/videos/072310-040-A/karambolage/';
+        $API_URL = 'https://api.arte.tv/api/player/v1/config/fr/072310-040-A';
+        $MEDIA_FILE_URL = 'https://arteptweb-a.akamaihd.net/am/ptweb/072000/072300/072310-040-A_SQ_0_VOF_03389080_MP4-2200_AMM-PTWEB_sPsY1JgixL.mp4';
+
+        $logger = $this->createMock(Logger::class);
+        $curl = $this->createMock(Curl::class);
+        $tools = new Tools($logger, $curl);
+
+        $curl->expects($this->exactly(2))
+        ->method('request')
+        ->withConsecutive(
+            [$this->equalTo($VALID_DOWNLOAD_URL)],
+            [$this->equalTo($API_URL)]
+        )
+        ->willReturnOnConsecutiveCalls(
+            $this->getFixture('arte/fr/originFr/videoPage.html'),
+            $this->getFixture('arte/fr/originFr/apiResponse.json')
+        );
+
+        $arte = new Arte($logger, $tools);
+        $result = $arte->getDownloadInfo($VALID_DOWNLOAD_URL);
+
+        $this->assertInstanceOf(Result::class, $result);
+        $this->assertEquals($MEDIA_FILE_URL, $result->getUri());
+        $this->assertEquals('Karambolage', $result->getTitle());
+        $this->assertEquals('', $result->getEpisodeTitle());
+    }
+}
