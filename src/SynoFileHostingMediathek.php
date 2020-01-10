d54af1f91dc5a6ae5aa0a9fa4a70fd4294f44303
Daniel Gehn <me@theinad.com>
2020-01-10 20:02:21 +0100

    Increase patch version, update composer dependencies

--- src/SynoFileHostingMediathek.php
+++ src/SynoFileHostingMediathek.php
@@ -41,7 +41,7 @@ include_once __DIR__ . '/Mediatheken/DreiSat.php';
  * All public functions are required by Synology Download Station.
  *
  * @author Daniel Gehn <me@theinad.com>
- * @version 0.5.1
+ * @version 0.5.2
  * @copyright 2017-2019 Daniel Gehn
  * @license http://opensource.org/licenses/MIT Licensed under MIT License
  */
